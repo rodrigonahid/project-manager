88ffb87a81c780ab0d50ae5a9271d8581b108d79
rodrigonahid <rodrigonahid1@gmail.com>
2021-11-05 20:02:20 -0300

    Fixing update project bug

--- src/controllers/JobController.js
+++ src/controllers/JobController.js
@@ -1,14 +1,21 @@
+const Job = require("../model/Job");
+const profile = require("../model/Profile");
+const JobUtils = require("../utils/jobUtils");
+
 module.exports = {
   index(req, res) {
     // Ajustes no projeto
-    const updatedJobs = Job.data.map((job) => {
-      const remainingDays = Job.services.remainingDays(job);
+    const Jobs = Job.get();
+    const Profile = profile.get();
+
+    const updatedJobs = Jobs.map((job) => {
+      const remainingDays = JobUtils.remainingDays(job);
       const status = job.remainingDays <= 0 ? "done" : "progress";
       return {
         ...job,
         remainingDays,
         status,
-        budget: Job.services.calculateBudget(job, Profile.data.value_hour),
+        budget: JobUtils.calculateBudget(job, Profile.value_hour),
       };
     });
 
@@ -18,10 +25,12 @@ module.exports = {
     return res.render("project");
   },
   save(req, res) {
+    const Jobs = Job.get();
+
     const job = req.body;
-    const lastId = Job.data[Job.data.length - 1]?.id || 1;
-    Job.data.created_at = Date.now();
-    Job.data.push({
+    const lastId = Jobs[Jobs.length - 1]?.id || 1;
+    Jobs.created_at = Date.now();
+    Jobs.push({
       id: lastId + 1,
       project_name: job["project-name"],
       daily_hours: job["daily-hours"],
@@ -33,18 +42,18 @@ module.exports = {
   },
   show(req, res) {
     const jobId = req.params.id;
-    const job = Job.data.find((job) => Number(job.id) === Number(jobId));
+    const job = Jobs.find((job) => Number(job.id) === Number(jobId));
 
     if (!job) {
       return res.send("Project not found");
     }
-    job.budget = Job.services.calculateBudget(job, Profile.data.value_hour);
+    job.budget = JobUtils.calculateBudget(job, Profile.data.value_hour);
 
     return res.render("project-edit", { job });
   },
   update(req, res) {
     const jobId = req.params.id;
-    const job = Job.data.find((job) => Number(job.id) === Number(jobId));
+    const job = Jobs.find((job) => Number(job.id) === Number(jobId));
 
     if (!job) {
       return res.send("Project not found");
@@ -57,7 +66,7 @@ module.exports = {
       daily_hours: req.body["daily-hours"],
     };
 
-    Job.data = Job.data.map((job) => {
+    Jobs = Jobs.map((job) => {
       if (Number(job.id) === Number(jobId)) {
         job = updatedJob;
       }
@@ -68,7 +77,7 @@ module.exports = {
   delete(req, res) {
     const jobId = req.params.id;
     console.log(req.param.id);
-    Job.data = Job.data.filter((job) => Number(job.id) !== Number(jobId));
+    Jobs = Jobs.filter((job) => Number(job.id) !== Number(jobId));
 
     return res.redirect("/");
   },
